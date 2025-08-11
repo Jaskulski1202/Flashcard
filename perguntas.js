4e739dfa3bce5ef513a4d79f0440668c8b190c66
Jaskulski1202 <jaskulski.silva@escola.pr.gov.br>
2025-08-11 11:30:03 -0300

    pergunta

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'matematica',
+    'Quais são as Aritmética básica? ',
+    '(adição, subtração, multiplicação, divisão)'
+)
+
+criaCartao(
+    'Quimica',
+    'Química Geral',
+    'Tabela periódica, ligações químicas, balanceamento de equações.'
+)
+
+criaCartao(
+    'fisica',
+    'Mecânica',
+    'Leis de Newton, movimento, energia e trabalho.'
+)
+    criaCartao(
+    'historia',
+    'História Antiga',
+    'Egito, Roma, Grécia.'
+)
